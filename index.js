202f340f6559f359bac01bce4618641912481b22
Ram Jeeva Sajan I <sajanilayaraja007@gmail.com>
2024-10-18 01:56:07 +0530

    New

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const DB_NAME = "movie_db";
+
+const COLLECTION_NAME = "movies";
+
+app.use(cors({
+  origin: "*"
+}));
+
+// Step 1. Connect the Database
+app.get("/movie/get-movies", async (req, res) => {
+  try {
+    const client = new MongoClient("mongodb+srv://sajanilayaraja007:1KbvzvzSPMomppr8@cluster0.axgm8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {}).connect()
+
+    // Step 2. Select the DB
+    let db = (await client).db(DB_NAME);
+
+    // Step 3. Select the Collectio
+    let collection = await db.collection(COLLECTION_NAME);
+
+    // Step 4. Do the operation
+    let movies = await collection.find({}).toArray();
+
+    // Step 5. Close the connection
+    (await client).close();
+
+    res.json(movies)
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+
+
+app.get("/movie/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    // Step 1. Connect the Database
+    const client = new MongoClient("mongodb+srv://sajanilayaraja007:1KbvzvzSPMomppr8@cluster0.axgm8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {}).connect();
+
+    // Step 2. Select the DB
+    let db = (await client).db(DB_NAME);
+
+    // Step 3. Select the Collection
+    let dbcollection = await db.collection(COLLECTION_NAME);
+
+    let movie = await dbcollection.findOne({ _id: new ObjectId(id) });
+
+    (await client).close();
+
+    res.json(movie);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+
+app.post("/book-ticket", (req, res) => {
+
+});
+
+app.listen(7000);
